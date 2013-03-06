ca44b49bfa45f673176c54b9ea3e9edc01c5709d
TTChangeTheWorld <TTChangeTheWorld@gmail.com>
2013-03-06 22:04:39 +0400

    0.13a (Nice shadow of ball added)

--- Scripts/core.js
+++ Scripts/core.js
@@ -58,7 +58,7 @@ function Ball(){
     }
     this.drawShadow = function(){
         field.save();
-        field.fillStyle="#444444";
+        field.fillStyle="rgba(50,50,50,0.5)";
         field.beginPath();
         field.scale(2,1);
         field.arc(this.x/2, field.canvas.height-this.r,Math.max(10,this.r*(field.canvas.height-this.y)/800),  0,Math.PI*2);
